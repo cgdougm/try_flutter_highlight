65834b420f8824beb81ab8000539e837050477f6
Doug MacMillan <github@raette.com>
2024-11-23 10:45:24 -0800

    added menu for language samples

--- lib/my_widget.dart
+++ lib/my_widget.dart
@@ -2,10 +2,40 @@ import 'package:flutter/material.dart';
 import 'package:flutter_highlight/flutter_highlight.dart';
 import 'package:flutter_highlight/themes/zenburn.dart';
 
-class MyWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    var code = '''
+const Map<String, String> sampleCodes = {
+  'Markdown': '''
+# Hello, World!
+
+## This is a second level heading
+
+> This is a block quote.
+>
+> It can span multiple paragraphs,
+> if you like.
+
+* This is a list item.
+* This is another one.
+
+---
+
+Here is some inline \`code\`.
+
+\`\`\`dart
+main() {
+  print("Hello, World!");
+}
+\`\`\`
+
+## Tables
+
+| Tables        | Are           | Cool  |
+| ------------- |:-------------:| -----:|
+| col 3 is      | right-aligned | \$1600 |
+| col 2 is      | centered      | \$12    |
+| zebra stripes | are neat      | \$1    |
+
+''',
+  'Dart': '''
 main() {
   print("Hello, World!");
   
@@ -28,29 +58,52 @@ main() {
       fontSize: 16,
     ),
   );
+}''',
+};
+
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
 }
-''';
 
-    return HighlightView(
-      // The original code to be highlighted
-      code,
+class _MyWidgetState extends State<MyWidget> {
+  String selectedLanguage = 'Markdown';
 
-      // Specify language
-      // It is recommended to give it a value for performance
-      language: 'dart',
-
-      // Specify highlight theme
-      // All available themes are listed in `themes` folder
-      theme: zenburnTheme,
-
-      // Specify padding
-      padding: EdgeInsets.all(12),
-
-      // Specify text style
-      textStyle: TextStyle(
-        fontFamily: 'Courier New',
-        fontSize: 16,
-      ),
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        PopupMenuButton<String>(
+          initialValue: selectedLanguage,
+          onSelected: (String language) {
+            setState(() {
+              selectedLanguage = language;
+            });
+          },
+          itemBuilder: (BuildContext context) {
+            return sampleCodes.keys.map((String language) {
+              return PopupMenuItem<String>(
+                value: language,
+                child: Text(language),
+              );
+            }).toList();
+          },
+        ),
+        Expanded(
+          child: HighlightView(
+            sampleCodes[selectedLanguage]!,
+            language: selectedLanguage.toLowerCase(),
+            theme: zenburnTheme,
+            padding: const EdgeInsets.all(12),
+            textStyle: const TextStyle(
+              fontFamily: 'Courier New',
+              fontSize: 16,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
