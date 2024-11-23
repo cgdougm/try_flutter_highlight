617e25ec9a17bbfc601ed4e8ecaaa44ddf898a6e
Doug MacMillan <github@raette.com>
2024-11-23 10:46:53 -0800

    menu for theme

--- lib/my_widget.dart
+++ lib/my_widget.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_highlight/flutter_highlight.dart';
 import 'package:flutter_highlight/themes/zenburn.dart';
+import 'package:flutter_highlight/themes/github.dart';
+import 'package:flutter_highlight/themes/dracula.dart';
+import 'package:flutter_highlight/themes/vs.dart';
+import 'package:flutter_highlight/themes/atom-one-dark.dart';
 
 const Map<String, String> sampleCodes = {
   'Markdown': '''
@@ -61,6 +65,14 @@ main() {
 }''',
 };
 
+const Map<String, Map<String, TextStyle>> highlightThemes = {
+  'Zenburn': zenburnTheme,
+  'GitHub': githubTheme,
+  'Dracula': draculaTheme,
+  'VS': vsTheme,
+  'Atom One Dark': atomOneDarkTheme,
+};
+
 class MyWidget extends StatefulWidget {
   const MyWidget({super.key});
 
@@ -70,32 +82,74 @@ class MyWidget extends StatefulWidget {
 
 class _MyWidgetState extends State<MyWidget> {
   String selectedLanguage = 'Markdown';
+  String selectedTheme = 'Zenburn';
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        PopupMenuButton<String>(
-          initialValue: selectedLanguage,
-          onSelected: (String language) {
-            setState(() {
-              selectedLanguage = language;
-            });
-          },
-          itemBuilder: (BuildContext context) {
-            return sampleCodes.keys.map((String language) {
-              return PopupMenuItem<String>(
-                value: language,
-                child: Text(language),
-              );
-            }).toList();
-          },
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            PopupMenuButton<String>(
+              initialValue: selectedLanguage,
+              onSelected: (String language) {
+                setState(() {
+                  selectedLanguage = language;
+                });
+              },
+              itemBuilder: (BuildContext context) {
+                return sampleCodes.keys.map((String language) {
+                  return PopupMenuItem<String>(
+                    value: language,
+                    child: Text(language),
+                  );
+                }).toList();
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(selectedLanguage),
+                    const Icon(Icons.arrow_drop_down),
+                  ],
+                ),
+              ),
+            ),
+            PopupMenuButton<String>(
+              initialValue: selectedTheme,
+              onSelected: (String theme) {
+                setState(() {
+                  selectedTheme = theme;
+                });
+              },
+              itemBuilder: (BuildContext context) {
+                return highlightThemes.keys.map((String theme) {
+                  return PopupMenuItem<String>(
+                    value: theme,
+                    child: Text(theme),
+                  );
+                }).toList();
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(selectedTheme),
+                    const Icon(Icons.arrow_drop_down),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
         Expanded(
           child: HighlightView(
             sampleCodes[selectedLanguage]!,
             language: selectedLanguage.toLowerCase(),
-            theme: zenburnTheme,
+            theme: highlightThemes[selectedTheme]!,
             padding: const EdgeInsets.all(12),
             textStyle: const TextStyle(
               fontFamily: 'Courier New',
